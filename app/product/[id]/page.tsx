0d486f749763e930d9993f849413062e9daa905f
myphone3 <d0556771356@gmail.com>
2026-08-31 12:30:02 +0300

    Update page.tsx

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../lib/supabase';
+import { supabase } from '../../../lib/supabase';
+
 import { useParams } from 'next/navigation';
 
 export default function ProductDetailPage() {
