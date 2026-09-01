2f1e69f4ec4a9d74cd693ad81716ffd1a9f5e687
myphone3 <d0556771356@gmail.com>
2026-09-01 04:39:41 +0300

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,8 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { supabase } from '../../../lib/supabase';
-
+import { supabase } from '@/lib/supabase';
 import Link from 'next/link';
 
 export default function CartPage() {
